586ca79a4d98594d086e657fbdcc275f22ea44fb
oferro <ofer.rotshtein@gmial.com>
2021-08-01 00:27:38 +0300

    add some props on the Rank

--- src/App.js
+++ src/App.js
@@ -35,7 +35,24 @@ class App extends Component {
       box: {},
       route: "signin",
       isSignedin: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: '',
+        joined: ''
+      }
     };
+  }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
   }
 
   calculateFaceLocation = (data) => {
@@ -88,7 +105,10 @@ class App extends Component {
         {route === "home" ? (
           <div>
             <Logo />
-            <Rank />
+            <Rank 
+              name = {this.state.user.name}
+              entries = {this.state.user.entries}
+            />
             <ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
@@ -100,9 +120,15 @@ class App extends Component {
           </div>
         ) : (route === 'signin' 
             ? 
-              <Signin onRouteChange={this.onRouteChange} />
+              <Signin 
+                loadUser={this.loadUser} 
+                onRouteChange={this.onRouteChange} 
+              />
             :
-              <Register onRouteChange={this.onRouteChange} />
+              <Register 
+                loadUser={this.loadUser} 
+                onRouteChange={this.onRouteChange} 
+              />
         )}
       </div>
     );
